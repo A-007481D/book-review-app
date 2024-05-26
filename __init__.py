8f845adaf33e71c4bcc6aa41cdf2409bb021bb14
A-007481D <labidabdelmalek@gmail.com>
2024-05-26 21:48:41 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-from . import routes #import routes after creating the app instance
-app.register_blueprint(routes.bp)
-
-def create_app():
-    return app
